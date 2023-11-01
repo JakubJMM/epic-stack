2c9277b2d9c73765420c671945c98ad23453349b
JakubJMM <Jakubmielcarek.jm@gmail.com>
2023-11-01 22:49:31 +0000

    nothing is working please help
    22:49

--- dobumartial/app/routes/_marketing+/index.tsx
+++ dobumartial/app/routes/_marketing+/index.tsx
@@ -5,18 +5,28 @@ import {
 	TooltipProvider,
 	TooltipTrigger,
 } from '#app/components/ui/tooltip.tsx'
-import { logos, stars } from './logos/logos.ts'
+import {
+	NavigationMenu,
+	NavigationMenuContent,
+	NavigationMenuIndicator,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
+	NavigationMenuTrigger,
+	NavigationMenuViewport,
+  } from '#app/components/ui/navigation-menu.tsx'
 
-export const meta: MetaFunction = () => [{ title: 'Epic Notes' }]
 
+export const meta: MetaFunction = () => [{ title: 'DobuMartial Arts' }]
 export default function Index() {
 	return (
 		<main className="relative min-h-screen sm:flex sm:items-center sm:justify-center">
+			
 			<div className="relative sm:pb-16 sm:pt-8">
 				<div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
 					<div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
 						<div className="absolute inset-0">
-							<img className="h-full w-full object-cover" src={stars} alt="" />
+							<img className="h-full w-full object-cover" alt="" />
 							<div className="absolute inset-0 bg-[color:rgba(30,23,38,0.5)] mix-blend-multiply" />
 						</div>
 						<div className="lg:pt-18 relative px-4 pb-8 pt-8 sm:px-6 sm:pb-14 sm:pt-16 lg:px-8 lg:pb-20">
@@ -25,10 +35,10 @@ export default function Index() {
 									className="block uppercase text-white drop-shadow-md"
 									href="https://www.epicweb.dev/stack"
 								>
-									<span>Epic Stack</span>
+									<span>DoBu Martial Arts</span>
 									<svg
 										className="mx-auto mt-2"
-										xmlns="http://www.w3.org/2000/svg"
+										xmlns="https://ibb.co/f0XDKn9"
 										width="120"
 										height="120"
 										fill="none"
@@ -56,29 +66,19 @@ export default function Index() {
 				</div>
 
 				<div className="mx-auto mt-8 max-w-7xl px-4 py-2 sm:px-6 lg:px-8">
-					<div className="flex flex-wrap justify-center gap-8 rounded-3xl bg-slate-100 py-4 dark:bg-slate-200">
-						<TooltipProvider>
-							{logos.map(img => (
-								<Tooltip key={img.href}>
-									<TooltipTrigger asChild>
-										<a
-											href={img.href}
-											className="flex h-16 w-32 justify-center p-1 grayscale transition hover:grayscale-0 focus:grayscale-0"
-										>
-											<img
-												alt={img.alt}
-												src={img.src}
-												className="object-contain"
-											/>
-										</a>
-									</TooltipTrigger>
-									<TooltipContent>{img.alt}</TooltipContent>
-								</Tooltip>
-							))}
-						</TooltipProvider>
+					<div className="flex flex-wrap justify-left gap-8 rounded-3xl bg-slate-100 py-4 dark:bg-slate-200">
+						This is the home page with info
 					</div>
 				</div>
 			</div>
+		
+		
+		
+		
+		
 		</main>
+
+
+
 	)
 }
